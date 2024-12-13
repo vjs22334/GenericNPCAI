701218cb488da28943237bcee0d9cfb9455bc791
jeyasurya <vjs122334@gmail.com>
2024-12-13 22:23:21 +0530

    added player hud and ammo count. removed auto reload

---
+++ Source/AIPlayground/Private/UtilityAI/Behaviours/ShootBehaviour.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UtilityAI/Behaviours/BaseBehaviour.h"
+#include "ShootBehaviour.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AIPLAYGROUND_API UShootBehaviour : public UBaseBehaviour
+{
+	GENERATED_BODY()
+};
