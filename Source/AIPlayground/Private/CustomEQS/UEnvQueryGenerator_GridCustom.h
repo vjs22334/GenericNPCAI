ae71f149c6be212ac95a45b304af3289edf289a2
jeyasurya <vjs122334@gmail.com>
2025-01-03 20:43:31 +0530

    added a custom eqs query generator

---
+++ Source/AIPlayground/Private/CustomEQS/UEnvQueryGenerator_GridCustom.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EnvironmentQuery/Generators/EnvQueryGenerator_ProjectedPoints.h"
+#include "UEnvQueryGenerator_GridCustom.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AIPLAYGROUND_API UUEnvQueryGenerator_GridCustom : public UEnvQueryGenerator_ProjectedPoints
+{
+	GENERATED_BODY()
+};
