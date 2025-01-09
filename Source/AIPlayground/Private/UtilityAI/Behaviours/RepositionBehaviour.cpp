c4c4edfab2e5c78a1d70c9152ec73a07ccc8d408
jeyasurya <vjs122334@gmail.com>
2025-01-09 22:37:30 +0530

    added crosshair, fixed issue with reposition ammo precondition not allowing reload

--- Source/AIPlayground/Private/UtilityAI/Behaviours/RepositionBehaviour.cpp
+++ Source/AIPlayground/Private/UtilityAI/Behaviours/RepositionBehaviour.cpp
@@ -113,7 +113,7 @@ bool URepositionBehaviour::IsAmmoInClip()
 {
 	if (M_GoalOwner->Implements<UWeaponInterface>())
 	{
-		if (IWeaponInterface::Execute_GetPercentAmmoLeftInClip(GoalOwner) > 50)
+		if (IWeaponInterface::Execute_GetPercentAmmoLeftInClip(GoalOwner) >= 30)
 		{
 			return true;
 		}
